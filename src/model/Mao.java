6368cc7ceeeacda61354fd352e273118ccf34377
hlbar <hlbar@192.168.15.5>
2025-01-09 21:15:54 -0300

    add comentarios aos teste e tirei o public das clases mao maojogador e carta

--- src/model/Mao.java
+++ src/model/Mao.java
@@ -2,22 +2,22 @@ package model;
 
 import java.util.*;
 
-public class Mao {
+ class Mao {
 
     private ArrayList<Carta> lista_cartas;
 
-    public Mao() {	//construtor
+    Mao() {	//construtor
         this.lista_cartas = new ArrayList<>();  // cria uma lista para as cartas na mao						//mao nao passou e não é 21 em valor ainda
     }
 
-    public Mao(List<Carta> cartas){
+    Mao(List<Carta> cartas){
         this.lista_cartas = (ArrayList<Carta>) cartas;
     }
-    public void addCarta(Carta carta) {
+    void addCarta(Carta carta) {
         lista_cartas.add(carta);
     }
 
-    public int calculaValorMao() {
+    int calculaValorMao() {
         int total = 0;
         int numAs = 0;
         for(Carta carta : lista_cartas) {
@@ -33,15 +33,15 @@ public class Mao {
         return total;
     }
 
-    public void limpaMao() {
+    void limpaMao() {
         lista_cartas.clear();
     }
 
-    public ArrayList<Carta> getLista_cartas() {
+    ArrayList<Carta> getLista_cartas() {
         return lista_cartas;
     }
 
-    public boolean estourou() {
+    boolean estourou() {
         return this.calculaValorMao() >= 21;
     }
 
--- src/model/MaoJogador.java
+++ src/model/MaoJogador.java
@@ -2,30 +2,30 @@ package model;
 
 import java.util.*;
 
-public class MaoJogador extends Mao {
+ class MaoJogador extends Mao {
 
     private int aposta;
 
-    public MaoJogador() {
+    MaoJogador() {
         super();
         aposta = 0;
 
     }
 
-    public MaoJogador(List<Carta> cartas, int aposta) {
+    private MaoJogador(List<Carta> cartas, int aposta) {
         super(cartas);
         this.aposta = aposta;
     }
 
-    public void aumentarAposta(int valor) {
+    void aumentarAposta(int valor) {
         aposta += valor;
     }
 
-    public void setAposta(int aposta) {
+    void setAposta(int aposta) {
         this.aposta = aposta;
     }
 
-    public int getAposta() {
+    int getAposta() {
         return aposta;
     }
 
